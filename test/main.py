726e83418fdaab22c6f6ad38a08b72eca3e04e64
CodyAirey <cody_airey@icloud.com>
2025-09-06 15:23:33 +1200

    UI test on gamestart

---
+++ test/main.py
@@ -0,0 +1,10 @@
+# main.py
+from loader import load_initial_state
+from ui import init_ui
+
+if __name__ == "__main__":
+    # state = load_initial_state()
+    # run_ui(state)
+
+    state = load_initial_state(4)
+    init_ui(state)
